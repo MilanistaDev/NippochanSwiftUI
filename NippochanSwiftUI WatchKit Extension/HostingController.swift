c65e27fd310aee4a3ef78694245bb3bfff7fe282
Takuya Aso <milanista224@me.com>
2019-07-20 10:18:07 +0900

    Initial Commit

---
+++ NippochanSwiftUI WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  NippochanSwiftUI WatchKit Extension
+//
+//  Created by Takuya Aso on 2019/07/20.
+//  Copyright © 2019 Takuya Aso. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ NippochanSwiftUI WatchKit Extension/NotificationView.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationView.swift
+//  NippochanSwiftUI WatchKit Extension
+//
+//  Created by Takuya Aso on 2019/07/20.
+//  Copyright © 2019 Takuya Aso. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
+#endif
