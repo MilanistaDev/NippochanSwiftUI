579abefc7cd8fb501c1cdfc8dc8d2a5621101ccd
Takuya Aso <milanista224@gmail.com>
2019-07-20 12:27:34 +0900

    Implement List

--- NippochanSwiftUI WatchKit Extension/SendActivityView.swift
+++ NippochanSwiftUI WatchKit Extension/SendActivityView.swift
@@ -9,8 +9,18 @@
 import SwiftUI
 
 struct SendActivityView: View {
+
+    let activityData: [String] = ["出社", "作業開始", "お昼休憩", "作業再開", "離席", "作業終了", "早退", "Add Activity"]
+    @State var isShown = false
+
     var body: some View {
-        Text("私はここにいる")
+        VStack(alignment: .leading) {
+            List(0..<self.activityData.count) { number in
+                Text(self.activityData[number])
+            }
+            .listStyle(.carousel)
+        }
+        .navigationBarTitle(Text("Activity"))
     }
 }
 
