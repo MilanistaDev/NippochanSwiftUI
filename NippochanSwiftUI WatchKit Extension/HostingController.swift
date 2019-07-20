7d98ee68f53f77a84f2602c0cd84f8a06c0b4c23
Takuya Aso <milanista224@gmail.com>
2019-07-20 11:15:40 +0900

    Change first screen view.

--- NippochanSwiftUI WatchKit Extension/HostingController.swift
+++ NippochanSwiftUI WatchKit Extension/HostingController.swift
@@ -10,8 +10,8 @@ import WatchKit
 import Foundation
 import SwiftUI
 
-class HostingController: WKHostingController<ContentView> {
-    override var body: ContentView {
-        return ContentView()
+class HostingController: WKHostingController<SendActivityView> {
+    override var body: SendActivityView {
+        return SendActivityView()
     }
 }
---
+++ NippochanSwiftUI WatchKit Extension/SendActivityView.swift
@@ -0,0 +1,23 @@
+//
+//  SendActivityView.swift
+//  NippochanSwiftUI WatchKit Extension
+//
+//  Created by Takuya Aso on 2019/07/20.
+//  Copyright © 2019 Takuya Aso. All rights reserved.
+//
+
+import SwiftUI
+
+struct SendActivityView: View {
+    var body: some View {
+        Text("私はここにいる")
+    }
+}
+
+#if DEBUG
+struct SendActivityView_Previews: PreviewProvider {
+    static var previews: some View {
+        SendActivityView()
+    }
+}
+#endif
