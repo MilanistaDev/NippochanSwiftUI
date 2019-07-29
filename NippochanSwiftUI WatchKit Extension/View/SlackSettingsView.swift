5ba20389b35249f8941e8876e7ee10c729dc86d2
Takuya Aso <milanista224@gmail.com>
2019-07-29 20:51:57 +0900

    Implemented Slack Setting screen.(on the way)

--- NippochanSwiftUI WatchKit Extension/View/SlackSettingsView.swift
+++ NippochanSwiftUI WatchKit Extension/View/SlackSettingsView.swift
@@ -9,8 +9,64 @@
 import SwiftUI
 
 struct SlackSettingsView: View {
+
+    @State private var webHookUrl: String = ""
+    @State private var gitHubLink: String = ""
+    @State private var favoriteColorHex: String = ""
+
+    private enum FieldType: Int {
+        case a
+        case b
+        case c
+    }
+
     var body: some View {
-        Text("Slack Settings")
+        ScrollView {
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Webhook URL")
+                        .font(.subheadline)
+                        .padding(Edge.Set.top, 2.0)
+                    Button(action: {
+                        print("Show Description for Slack Webhook.")
+                    }) {
+                        Image("ic_help")
+                    }
+                    .frame(width: 20.0, height: 20.0)
+                    .cornerRadius(10.0)
+                }
+                TextField("https://hooks.slack...", text: $webHookUrl)
+
+                HStack {
+                    Text("GitHub URL")
+                        .font(.subheadline)
+                        .padding(Edge.Set.top, 2.0)
+                    Button(action: {
+                        print("Show Description for GitHub Link and explain how to be used.")
+                    }) {
+                        Image("ic_help")
+                    }
+                    .frame(width: 20.0, height: 20.0)
+                    .cornerRadius(10.0)
+                }
+                TextField("https://github...", text: $gitHubLink)
+
+                HStack {
+                    Text("Favorite Color")
+                        .font(.subheadline)
+                        .padding(Edge.Set.top, 2.0)
+                    Button(action: {
+                        print("Show Description for favorite Color.")
+                    }) {
+                        Image("ic_help")
+                    }
+                    .frame(width: 20.0, height: 20.0)
+                    .cornerRadius(10.0)
+                }
+                TextField("#009944", text: $favoriteColorHex)
+            }
+            .navigationBarTitle(Text("Slack Settings"))
+        }
     }
 }
 
