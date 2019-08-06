ab9fbf8d4f71694d4ba52474f1b44d3fceaf408f
Takuya Aso <milanista224@gmail.com>
2019-08-07 02:22:55 +0900

    Implemented info buttons action.

--- NippochanSwiftUI WatchKit Extension/View/SlackSettingsView.swift
+++ NippochanSwiftUI WatchKit Extension/View/SlackSettingsView.swift
@@ -8,6 +8,12 @@
 
 import SwiftUI
 
+enum SettingType {
+    case webhook
+    case github
+    case favColor
+}
+
 struct SlackSettingsView: View {
 
     @State private var webHookUrl: String = ""
@@ -17,41 +23,61 @@ struct SlackSettingsView: View {
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
-                LabelTextField(title: "Webhook URL",
-                               placeHolder: "https://hooks.slack...",
-                               text: $webHookUrl)
-                LabelTextField(title: "GitHub URL",
-                               placeHolder: "https://github.com/MilanistaDev",
-                               text: $gitHubLink)
-                LabelTextField(title: "Favorite Color",
-                               placeHolder: "#009944",
-                               text: $favoriteColorHex)
+                SlackSettingContentsView(title: "Webhook URL",
+                                         placeHolder: "https://hooks.slack...",
+                                         text: $webHookUrl,
+                                         type: .webhook)
+                SlackSettingContentsView(title: "GitHub URL",
+                                         placeHolder: "https://github.com/MilanistaDev",
+                                         text: $gitHubLink,
+                                         type: .github)
+                SlackSettingContentsView(title: "Favorite Color",
+                                         placeHolder: "#009944",
+                                         text: $favoriteColorHex,
+                                         type: .favColor)
             }
             .navigationBarTitle(Text("Slack Settings"))
         }
     }
 }
 
-struct LabelTextField: View {
+struct SlackSettingContentsView: View {
 
     var title: String
     var placeHolder: String
     var text: Binding<String>
+    var type: SettingType
+    @State var isPresented = false
 
     var body: some View {
-        VStack(alignment: .leading) {
+        Group {
             HStack {
                 Text(title)
                     .font(.subheadline)
                     .padding(Edge.Set.top, 2.0)
                 Button(action: {
+                    self.isPresented.toggle()
                 }) {
                     Image("ic_help")
                 }
                 .frame(width: 20.0, height: 20.0)
                 .cornerRadius(10.0)
+                .sheet(isPresented: $isPresented, content: {
+                    Text(self.showDescription(type: self.type))
+                })
             }
             TextField(placeHolder, text: text)
+        }
+    }
+
+    private func showDescription(type: SettingType) -> String {
+        switch type {
+        case .webhook:
+            return "Description for webhook"
+        case .github:
+            return "Description for GitHub"
+        case .favColor:
+            return "Description for Favorite Color"
         }
     }
 }
