d86871cf35400af7735cb5d84f15cdf4bb024724
Takuya Aso <milanista224@gmail.com>
2019-08-07 02:12:08 +0900

    Extracting the Label and Text Field for Reuse.

--- NippochanSwiftUI WatchKit Extension/View/SlackSettingsView.swift
+++ NippochanSwiftUI WatchKit Extension/View/SlackSettingsView.swift
@@ -14,58 +14,44 @@ struct SlackSettingsView: View {
     @State private var gitHubLink: String = ""
     @State private var favoriteColorHex: String = ""
 
-    private enum FieldType: Int {
-        case a
-        case b
-        case c
-    }
-
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
-                HStack {
-                    Text("Webhook URL")
-                        .font(.subheadline)
-                        .padding(Edge.Set.top, 2.0)
-                    Button(action: {
-                        print("Show Description for Slack Webhook.")
-                    }) {
-                        Image("ic_help")
-                    }
-                    .frame(width: 20.0, height: 20.0)
-                    .cornerRadius(10.0)
-                }
-                TextField("https://hooks.slack...", text: $webHookUrl)
-
-                HStack {
-                    Text("GitHub URL")
-                        .font(.subheadline)
-                        .padding(Edge.Set.top, 2.0)
-                    Button(action: {
-                        print("Show Description for GitHub Link and explain how to be used.")
-                    }) {
-                        Image("ic_help")
-                    }
-                    .frame(width: 20.0, height: 20.0)
-                    .cornerRadius(10.0)
-                }
-                TextField("https://github...", text: $gitHubLink)
-
-                HStack {
-                    Text("Favorite Color")
-                        .font(.subheadline)
-                        .padding(Edge.Set.top, 2.0)
-                    Button(action: {
-                        print("Show Description for favorite Color.")
-                    }) {
-                        Image("ic_help")
-                    }
-                    .frame(width: 20.0, height: 20.0)
-                    .cornerRadius(10.0)
-                }
-                TextField("#009944", text: $favoriteColorHex)
+                LabelTextField(title: "Webhook URL",
+                               placeHolder: "https://hooks.slack...",
+                               text: $webHookUrl)
+                LabelTextField(title: "GitHub URL",
+                               placeHolder: "https://github.com/MilanistaDev",
+                               text: $gitHubLink)
+                LabelTextField(title: "Favorite Color",
+                               placeHolder: "#009944",
+                               text: $favoriteColorHex)
             }
             .navigationBarTitle(Text("Slack Settings"))
+        }
+    }
+}
+
+struct LabelTextField: View {
+
+    var title: String
+    var placeHolder: String
+    var text: Binding<String>
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Text(title)
+                    .font(.subheadline)
+                    .padding(Edge.Set.top, 2.0)
+                Button(action: {
+                }) {
+                    Image("ic_help")
+                }
+                .frame(width: 20.0, height: 20.0)
+                .cornerRadius(10.0)
+            }
+            TextField(placeHolder, text: text)
         }
     }
 }
